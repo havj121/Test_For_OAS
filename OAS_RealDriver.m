d075e1350af2c37ab4c077e72ea638de594fbb6a
havj121 <ranwei@tongji.edu.cn>
2022-01-17 14:45:15 +0800

    add properties driving miles

--- OAS_RealDriver.m
+++ OAS_RealDriver.m
@@ -2,31 +2,36 @@ classdef OAS_RealDriver < OAS_Driver
     %This class defines a real driver which contains only a method
     
     properties
-        ID='RealDriver'
         % Basic information of the driver
-        Name
+        Name='RealDriver'
         Age
         Gender
         PersonalTraits
+        DrivingMilesPerYear
     end
     
     methods
-        function obj = OAS_RealDriver(Name,Gender,Age)
+        function obj = OAS_RealDriver(Name,Age,DrivingMilesPerYear,Gender)
             % Constructor
             if nargin==1
                 obj.Name=Name;
             elseif nargin==2
                 obj.Name=Name;
-                obj.Gender=Gender;
+                obj.Age=Age;
             elseif nargin==3
                 obj.Name=Name;
-                obj.Gender=Gender;
                 obj.Age=Age;
+                obj.DrivingMilesPerYear=DrivingMilesPerYear;
+            elseif nargin==4
+                obj.Name=Name;
+                obj.Age=Age;
+                obj.DrivingMilesPerYear=DrivingMilesPerYear;
+                obj.Gender=Gender;
             end
             obj.Driver_Type="Real";
             % register this driver object in the oas_handlemanager
-            OAS_handlemanager=OAS_HandleManager.getInstance();
-            OAS_handlemanager.register(obj.ID,obj);
+%             OAS_handlemanager=OAS_HandleManager.getInstance();
+%             OAS_handlemanager.register(obj.Name,obj);
         end
         
         function Evaluation = Evaluate(obj,Trajectory)
@@ -35,6 +40,7 @@ classdef OAS_RealDriver < OAS_Driver
             Evaluation.Type="Compare";
             Evaluation.Result=0;
         end
+        
     end
 end
 
